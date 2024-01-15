ea54783a9143efceca825bd0b8877ea620e42254
vishnubhargavd <vishnubhargavd@gmail.com>
2024-01-15 10:00:00 +0530

    feat: add user registration service

---
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -0,0 +1,15 @@
+package com.fitness.userservice.service;
+
+import org.springframework.stereotype.Service;
+
+import com.fitness.userservice.dto.*;
+import com.fitness.userservice.*;
+import com.fitness.userservice.Model.User;
+@Service
+public class UserService {
+    public UserService register(RegisterRequest request){
+        User user = new User();
+    }
+
+
+}
