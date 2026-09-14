75c82141335b9b9510d3fbcf825043579a5349e3
vishnubhargavd <vishnubhargavd@gmail.com>
2026-09-14 20:31:10 +0530

    fixed code

--- userservice/src/main/java/com/fitness/userservice/controller/usercontroller.java
+++ userservice/src/main/java/com/fitness/userservice/controller/usercontroller.java
@@ -1,36 +1,35 @@
 package com.fitness.userservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.fitness.userservice.service.*;
-
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-
-import private String email;
-private String password; private String firstName; private String lastName;;
-import com.fitness.userservice.dto.UserResponse;
 
 @RestController
-@RequestMapping ("/api/users")
+@RequestMapping("/api/users")
 @AllArgsConstructor
 public class usercontroller {
 
     private UserService userService;
 
-	@GetMapping ("/{userId}")
-	public ResponseEntity<UserResponse>resgister(@RequestBody RegisterRequest request){
-	return ResponseEntity.ResponseEntity.ok(request.register(request));
+    @GetMapping("/register")
+    public ResponseEntity<UserResponse> resgister(
+        @Valid @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(UserService.register(request));
+    }
 
-	}
-	@GetMapping ("/register")
-	public ResponseEntity<UserResponse>getUserProfile( @Valid @PathVariable String userId){
-	return ResponseEntity.ok(UserService.getUserProfile(userId));
-
-	}
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(
+        @PathVariable String userId
+    ) {
+        return ResponseEntity.ok(UserService.getUserProfile(userId));
+    }
 }
